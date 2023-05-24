176131bf6fd25e0c214c99d2f9db667a14d8ddfc
Rogério Luiz <rogerio.macdev@gmail.com>
2023-05-23 21:38:17 -0300

    🚀 Final adjustments

--- grp04/src/actions.ts
+++ grp04/src/actions.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, TicketType } from '@prisma/client';
+import { PrismaClient, Ticket, TicketType } from '@prisma/client';
 import { format } from 'date-fns';
 
 const priority = {
@@ -7,6 +7,22 @@ const priority = {
   SG: 2,
 };
 
+function sortTickets(tickets: Ticket[], priority: Ticket['role'][]) {
+  return tickets.sort((a, b) => {
+    if (a.role === priority[0]) {
+      return -1;
+    } else if (b.role === priority[0]) {
+      return 1;
+    } else if (a.role === priority[1]) {
+      return -1;
+    } else if (b.role === priority[1]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+}
+
 export const getWindows = async (prisma: PrismaClient) => {
   const windows = await prisma.window.findMany({
     include: {
@@ -14,31 +30,96 @@ export const getWindows = async (prisma: PrismaClient) => {
         select: {
           id: true,
           code: true,
-          created_at: true,
-          finished_at: true,
+          status: true,
           role: true,
+          created_at: true,
+          started_at: true,
+          finished_at: true,
         },
       },
     },
   });
 
-  const output = windows.map((window) => {
-    return {
-      ...window,
+  const output = [];
+
+  for await (const window of windows) {
+    output.push({
+      sg: await prisma.ticket.count({
+        where: { window_id: window.id, role: 'SG' },
+      }),
+      sp: await prisma.ticket.count({
+        where: { window_id: window.id, role: 'SP' },
+      }),
+      se: await prisma.ticket.count({
+        where: { window_id: window.id, role: 'SE' },
+      }),
       total: window.Ticket.length,
-    };
-  });
+      ...window,
+    });
+  }
 
   return output;
 };
 
-export const getPreviousTickets = async (prisma: PrismaClient) => {
+export const getStatus = async (prisma: PrismaClient) => {
+  const windows = await getWindows(prisma);
+
+  const ticketsRole = await prisma.ticket.groupBy({
+    by: ['role'],
+    _count: {
+      role: true,
+    },
+  });
+
+  const ticketsStatus = await prisma.ticket.groupBy({
+    by: ['status'],
+    _count: {
+      status: true,
+    },
+  });
+
+  const ticketCount = await prisma.ticket.count();
+
+  return {
+    windows,
+    metrics: {
+      byRole: ticketsRole.map((ticket) => {
+        return {
+          title: ticket.role,
+          count: ticket._count.role,
+        };
+      }),
+      byStatus: ticketsStatus.map((ticket) => {
+        return {
+          title: ticket.status,
+          count: ticket._count.status,
+        };
+      }),
+      total: ticketCount,
+    },
+  };
+};
+
+export const getTickets = async (prisma: PrismaClient) => {
   const tickets = await prisma.ticket.findMany({
+    include: {
+      window: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
     where: {
       NOT: {
         status: 'WAITING',
       },
     },
+    orderBy: {
+      started_at: 'desc',
+    },
+    skip: 0,
+    take: 5,
   });
 
   return tickets;
@@ -72,7 +153,7 @@ export const requestTicket = async (
       finished_at: new Date(),
     },
     where: {
-      id: window.id,
+      window_id: window.id,
       status: 'WORKING',
     },
   });
@@ -86,19 +167,29 @@ export const requestTicket = async (
     },
   });
 
-  const output = tickets.sort((a, b) => {
-    if (a.role === 'SE') {
-      return -1;
-    } else if (b.role === 'SE') {
-      return 1;
-    } else if (a.role === 'SP') {
-      return -1;
-    } else if (b.role === 'SP') {
-      return 1;
-    } else {
-      return 0;
-    }
+  if (tickets.length === 0) throw 'no tickets';
+
+  let output: Ticket[] = [];
+
+  const previousTicket = await prisma.ticket.findFirst({
+    where: {
+      window_id: window.id,
+      status: 'FINISHED',
+    },
+    orderBy: {
+      finished_at: 'desc',
+    },
   });
+
+  if (previousTicket) {
+    if (previousTicket.role === 'SP') {
+      output = sortTickets(tickets, ['SE', 'SG']);
+    } else {
+      output = sortTickets(tickets, ['SP', 'SP']);
+    }
+  } else {
+    output = sortTickets(tickets, ['SP', 'SE']);
+  }
 
   const ticket = await prisma.ticket.update({
     data: {
--- grp04/src/app.ts
+++ grp04/src/app.ts
@@ -1,24 +1,42 @@
 import express from 'express';
 import cors from 'cors';
+import WebSocket from 'ws';
 
+import { EventEmitter } from 'events';
 import { PrismaClient } from '@prisma/client';
 import { log } from 'console';
 import {
   createTicket,
-  getPreviousTickets,
+  getStatus,
+  getTickets,
   getWindows,
   requestTicket,
 } from './actions';
 
 const app = express();
+const server = new WebSocket.Server({ port: 3000 });
+
 const PORT = process.env.PORT;
 const prisma = new PrismaClient();
+const updateEmitter = new EventEmitter();
 
 const main = async () => {
   app.use(cors(), express.json());
 
   app.get('/', (_req, res) => {
     res.status(444).send();
+  });
+
+  app.get('/status', async (_req, res) => {
+    await getStatus(prisma)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(401).json({
+          err,
+        });
+      });
   });
 
   app.get('/windows', async (_req, res) => {
@@ -34,7 +52,7 @@ const main = async () => {
   });
 
   app.get('/tickets', async (_req, res) => {
-    await getPreviousTickets(prisma)
+    await getTickets(prisma)
       .then((data) => {
         res.json(data);
       })
@@ -71,6 +89,7 @@ const main = async () => {
     if (window) {
       await requestTicket(prisma, window)
         .then((data) => {
+          updateEmitter.emit('requestTicket', JSON.stringify(data));
           res.json(data);
         })
         .catch((err) => {
@@ -90,4 +109,20 @@ const main = async () => {
   });
 };
 
+const startWs = () => {
+  server.on('connection', (socket) => {
+    log('Novo cliente WebSocket conectado!');
+
+    updateEmitter.on('requestTicket', (ticket) => {
+      log(`Senha solicitada para atendimento`);
+      socket.send(ticket);
+    });
+
+    socket.on('close', () => {
+      log('Cliente WebSocket desconectado!');
+    });
+  });
+};
+
 main();
+startWs();

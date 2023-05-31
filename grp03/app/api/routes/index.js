18c63b17cb40f0252defa1b9bcde0d4591e92770
Pedro Williams <pedrowilliamsbc@gmail.com>
2023-05-30 21:26:52 -0300

    finalizando 4M9EC-SD 😁

--- grp03/app/api/routes/index.js
+++ grp03/app/api/routes/index.js
@@ -1,12 +1,18 @@
 /* eslint-disable arrow-parens */
 /* eslint-disable import/no-extraneous-dependencies */
 const bodyParser = require('body-parser');
-const tokenRouter = require('./tokenRouter');
+const senhaRouter = require('./senhaRouter');
+const atendimentoRouter = require('./atendimentoRouter');
+const guicheRouter = require('./guicheRouter');
+const relatorioRouter = require('./relatorioRouter');
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
     bodyParser.urlencoded({ extended: true }),
-    tokenRouter,
+    senhaRouter,
+    atendimentoRouter,
+    guicheRouter,
+    relatorioRouter,
   );
 };
